c3cb53208d7581f6df47f534655ab2b42d448606
johnnynu <johnnynu98@gmail.com>
2025-05-02 16:41:17 -0700

    setup routes complete

--- apollo/src/App.tsx
+++ apollo/src/App.tsx
@@ -1,5 +1,9 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import HomePage from "./pages/HomePage";
+import PostPage from "./pages/PostPage";
+import ProfilePage from "./pages/ProfilePage";
+import SubmitPage from "./pages/SubmitPage";
+import SubredditPage from "./pages/SubredditPage";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/layout";
 
@@ -10,6 +14,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
+            <Route path="r/:subredditName" element={<SubredditPage />} />
+            <Route path="post/:postId" element={<PostPage />} />
+            <Route path="u/:username" element={<ProfilePage />} />
+            <Route path="r/:subredditName/submit" element={<SubmitPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
       </BrowserRouter>
